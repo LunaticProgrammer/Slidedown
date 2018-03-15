bffd88c40cd95629b0954805f5eecb00cf34703a
LunaticProgrammer <30633466+LunaticProgrammer@users.noreply.github.com>
2018-03-15 14:34:38 +0530

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>http://www.kashishshah.com</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://www.kashishshah.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://www.kashishshah.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://www.kashishshah.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://www.kashishshah.com/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://www.kashishshah.com/index.php?rest_route=/" />
+	    </apis>
+  </service>
+</rsd>
